70c67f6a0accd1bcf894dc2bf97a901ec477981e
linoreki <148707553+linoreki@users.noreply.github.com>
2024-08-24 23:39:09 +0200

    Fix client.py and main with GUI

--- client/client.py
+++ client/client.py
@@ -5,6 +5,24 @@ import os
 from colorama import Fore
 import colorama
 
+def client():
+    global client_socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.settimeout(10)  # Establece un tiempo de espera para la conexión
+    try:
+        client_socket.connect((host, 5555))
+        client_socket.send(nickname.encode('utf-8'))
+
+        receive_thread = threading.Thread(target=receive)
+        receive_thread.start()
+
+        write_thread = threading.Thread(target=write)
+        write_thread.start()
+
+    except socket.error as e:
+        print(Fore.RED + f"Socket error: {e}")
+        sys.exit()
+
 def print_help():
     help_text = """
     Available commands:
@@ -17,16 +35,19 @@ def print_help():
 def receive():
     while True:
         try:
-            header = client.recv(1024).decode('utf-8')
+            header = client_socket.recv(1024).decode('utf-8')
+            if not header:  # Si no hay más datos, se asume que la conexión se ha cerrado
+                break
             if header.startswith('FILE'):
                 _, filename, file_size = header.split(':')
                 file_size = int(file_size)
                 print(Fore.BLUE + f"Receiving file: {filename} ({file_size} bytes)")
                 
-                # Recibir el archivo en fragmentos
                 with open(f"received_{filename}", "wb") as f:
                     while file_size > 0:
-                        data = client.recv(min(file_size, 4096))
+                        data = client_socket.recv(min(file_size, 4096))
+                        if not data:
+                            break
                         f.write(data)
                         file_size -= len(data)
                 
@@ -35,37 +56,39 @@ def receive():
                 print(Fore.CYAN + header)
         except Exception as e:
             print(Fore.RED + f"An error occurred: {e}")
-            client.close()
             break
+    client_socket.close()
 
-def write(client):
+def write():
     print_help()
     while True:
-        message = input(f'{nickname}: ')
-        if message.startswith("/sendfile"):
-            _, file_path = message.split(' ', 1)
-            send_file(file_path)
-        elif message == "/help":
-            print_help()
-        elif message == "/exit":
-            print(Fore.RED + "Exiting chat...")
-            client.close()
+        try:
+            message = input(f'{nickname}: ')
+            if message.startswith("/sendfile"):
+                _, file_path = message.split(' ', 1)
+                send_file(file_path)
+            elif message == "/help":
+                print_help()
+            elif message == "/exit":
+                print(Fore.RED + "Exiting chat...")
+                client_socket.close()
+                break
+            else:
+                client_socket.send(f"{nickname}: {message}".encode('utf-8'))
+        except Exception as e:
+            print(Fore.RED + f"An error occurred while sending: {e}")
             break
-        else:
-            client.send(f"{nickname}: {message}".encode('utf-8'))
 
-def send_file(file_path, client):
+def send_file(file_path):
     try:
         file_size = os.path.getsize(file_path)
-
         filename = os.path.basename(file_path)
-        client.send(f"FILE:{filename}:{file_size}".encode('utf-8'))
+        client_socket.send(f"FILE:{filename}:{file_size}".encode('utf-8'))
         print(Fore.YELLOW + f"Sending file: {filename} ({file_size} bytes)")
 
-        # Enviar el archivo en fragmentos de 4096 bytes
         with open(file_path, "rb") as f:
             while (data := f.read(4096)):
-                client.send(data)
+                client_socket.send(data)
         
         print(Fore.GREEN + f"File {filename} sent successfully!")
     except FileNotFoundError:
@@ -74,27 +97,15 @@ def send_file(file_path, client):
         print(Fore.RED + f"Failed to send file {file_path}: {e}")
 
 def main():
-
     colorama.init(autoreset=True)
+    global nickname
+    global host
+    global client_socket
 
-    host = input("Enter the server's IP: ") 
-    print(host)
+    host = input(Fore.GREEN + "\nEnter the server's IP: ")
     nickname = input(Fore.GREEN + "\nPlease enter your nickname: ")
 
-    if len(sys.argv) == 2:
-        host = sys.argv[1]
-
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((host, 5555))
-
-    # Send the nickname to the server immediately after connecting
-    client.send(nickname.encode('utf-8'))
-
-    receive_thread = threading.Thread(target=receive)
-    receive_thread.start()
-
-    write_thread = threading.Thread(target=write)
-    write_thread.start()
+    client()
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -1,32 +1,67 @@
-import sys
-import os
+import threading
+import queue
+import customtkinter as ctk
 from server import server
 from client import client, clientGUI
 import test
 import gen_key
 
+task_queue = queue.Queue()
+
+def execute_option(option):
+    try:
+        if option == 'Server':
+            server.main()
+        elif option == 'Client':
+            client.main()
+        elif option == 'Client GUI':
+            clientGUI.main()
+        elif option == 'Test':
+            test.main()
+        elif option == 'Key Generation':
+            gen_key.main()
+        else:
+            print("Invalid option.")
+    except Exception as e:
+        print(f"Error executing {option}: {e}")
+
+def run_in_thread(option):
+    thread = threading.Thread(target=execute_option, args=(option,))
+    thread.start()
+
+def check_queue():
+    while not task_queue.empty():
+        task = task_queue.get()
+        print(task)
+    app.after(100, check_queue) 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: main.py -s | -c | -g | -t | -l")
-        sys.exit(1)
+    global app
 
-    option = sys.argv[1]
+    app = ctk.CTk() 
+    app.title("Main Menu")
+    app.geometry("400x400")
 
-    print(option)
+    label = ctk.CTkLabel(app, text="Choose an option to execute", font=("Arial", 16))
+    label.pack(pady=20)
 
-    if option == '-s':
-        server.main()
-    elif option == '-c':
-        client.main()
-    elif option == '-g':
-        clientGUI.main()
-    elif option == '-t':
-        test.main()
-    elif option == '-k':
-        gen_key.main()
-    else:
-        print("Invalid option. Use -s for server, -c for client, -g for client GUI, -t for test, or -l for key generation.")
-        sys.exit(1)
+    button_server = ctk.CTkButton(app, text="Server", command=lambda: run_in_thread('Server'))
+    button_server.pack(pady=10)
+
+    button_client = ctk.CTkButton(app, text="Client", command=lambda: run_in_thread('Client'))
+    button_client.pack(pady=10)
+
+    button_gui = ctk.CTkButton(app, text="Client GUI", command=lambda: run_in_thread('Client GUI'))
+    button_gui.pack(pady=10)
+
+    button_test = ctk.CTkButton(app, text="Test", command=lambda: run_in_thread('Test'))
+    button_test.pack(pady=10)
+
+    button_keygen = ctk.CTkButton(app, text="Key Generation", command=lambda: run_in_thread('Key Generation'))
+    button_keygen.pack(pady=10)
+
+    app.after(100, check_queue)
+
+    app.mainloop()
 
 if __name__ == "__main__":
     main()
